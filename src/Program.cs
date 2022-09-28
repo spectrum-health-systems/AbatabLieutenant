84db2e6bf0919533805f0faa9c18e48b4891b2a5
Chris Banwarth <github@aprettycoolprogram.com>
2022-09-28 16:10:53 -0400

    Update

--- src/Program.cs
+++ src/Program.cs
@@ -15,11 +15,14 @@ namespace AbatabLieutenant
             //}
 
             var abatabTempDir = @"C:\AvatoolWebService\Abatab_Temp\";
+
             if (Directory.Exists(abatabTempDir))
             {
                 Directory.Delete(abatabTempDir, true);
                 Directory.CreateDirectory(abatabTempDir);
             }
+
+            Directory.CreateDirectory(abatabTempDir);
 
             //var repoUrl     = "https://github.com/spectrum-health-systems/Abatab/tree/development";
             //var repoZipfile = @"C:\AvatoolWebService\Abatar_Temp\Abatar-repo.zip";
