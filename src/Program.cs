bb4b97a6836a23937830860e1148ab3e9fb1b8cb
Chris Banwarth <github@aprettycoolprogram.com>
2022-09-28 18:27:16 -0400

    Update

--- src/Program.cs
+++ src/Program.cs
@@ -25,29 +25,48 @@ namespace AbatabLieutenant
     {
         public static string AbatabUatRoot = @"C:\AvatoolWebService\Abatab_UAT";
         public static string RepoUrl       = "https://github.com/spectrum-health-systems/Abatab/archive/refs/heads/development.zip";
+        public static string Timestamp     = DateTime.Now.ToString("yy-MM-dd_HH-mm-ss");
 
         static void Main(string[] args)
         {
-            var logFileName = $@"{AbatabUatRoot}\Logs\AbatabLieutenant.{DateTime.Now.ToString("yy-MM-dd_HH-mm-ss")}";
+            var logFileName = $@"{AbatabUatRoot}\Logs\AbatabLieutenant.{Timestamp}";
             File.WriteAllText(logFileName, $"Log file start...{Environment.NewLine}");
 
             var tempDir = $@"{AbatabUatRoot}\Temp";
             RefreshDirectory(tempDir, logFileName);
 
-            var repoZipName = $@"{tempDir}\Abatab-repo.zip";
-            DownloadZipFromUrl(RepoUrl, repoZipName, logFileName);
-
-            File.AppendAllText(logFileName, $@"Extracting {repoZipName}...{Environment.NewLine}");
-            ZipFile.ExtractToDirectory(repoZipName, tempDir);
-            File.AppendAllText(logFileName, $@"{repoZipName} extracted.{Environment.NewLine}");
-
             var webServiceBinDir = $@"{AbatabUatRoot}\bin";
             RefreshDirectory(webServiceBinDir, logFileName);
 
+            var repoZipName = $@"{tempDir}\Abatab-repo.zip";
+            DownloadZipFromUrl(RepoUrl, repoZipName, logFileName);
+
+            ExtractArchive(repoZipName, tempDir, logFileName);
+
+            CopyBinDirectory(tempDir, webServiceBinDir, logFileName);
+
+            CopyWebServiceFiles(tempDir, logFileName);
+
+            File.AppendAllText(logFileName, $"Process complete!{Environment.NewLine}");
+        }
+
+        private static void ExtractArchive(string repoZipName, string tempDir, string logFileName)
+        {
+            File.AppendAllText(logFileName, $@"Extracting {repoZipName}...{Environment.NewLine}");
+            ZipFile.ExtractToDirectory(repoZipName, tempDir);
+            File.AppendAllText(logFileName, $@"{repoZipName} extracted.{Environment.NewLine}");
+        }
+
+
+        private static void CopyBinDirectory(string tempDir, string webServiceBinDir, string logFileName)
+        {
             File.AppendAllText(logFileName, $@"Copying {tempDir}\Abatab-development\src\bin\ to {webServiceBinDir}...{Environment.NewLine}");
             CopyDir($@"{tempDir}\Abatab-development\src\bin\", $@"{webServiceBinDir}", logFileName);
             File.AppendAllText(logFileName, $@"{tempDir}\Abatab-development\src\bin\ copied to {webServiceBinDir}.{Environment.NewLine}");
+        }
 
+        private static void CopyWebServiceFiles(string tempDir, string logFileName)
+        {
             var filesToCopy = new List<string>
             {
                 "Abatab.asmx",
@@ -77,6 +96,7 @@ namespace AbatabLieutenant
                 File.AppendAllText(logFileName, $@"File: {sourcePath}\{file} copied.{Environment.NewLine}");
             }
             File.AppendAllText(logFileName, $"Web service files copied.{Environment.NewLine}");
+
         }
 
         private static void RefreshDirectory(string dir, string logFileName)
