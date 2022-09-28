1ed2e8e8d3ef84b3cac50b88f9fe5f6170cb630b
Chris Banwarth <github@aprettycoolprogram.com>
2022-09-28 16:19:59 -0400

    Update

--- src/Program.cs
+++ src/Program.cs
@@ -26,7 +26,7 @@ namespace AbatabLieutenant
 
             var repoUrl     = "https://github.com/spectrum-health-systems/Abatab/tree/development";
             //var repoZipfile = @"C:\AvatoolWebService\Abatar_Temp\Abatar-repo.zip";
-            var repoZipfile = @"C:\test\Abatar-repo.zip";
+            var repoZipfile = $@"{abatabTempDir}Abatar-repo.zip";
 
             DownloadZipFromUrl(repoUrl, repoZipfile);
 
