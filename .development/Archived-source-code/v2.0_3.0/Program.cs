b295d90654354f92a0fc97964c5863e46089c13e
Chris Banwarth <github@aprettycoolprogram.com>
2023-02-07 11:20:25 -0500

    Development resources

---
+++ .development/Archived-source-code/v2.0_3.0/Program.cs
@@ -0,0 +1,32 @@
+﻿
+
+using System;
+
+namespace AbatabLieutenant
+{
+    internal static class Program
+    {
+        internal static void Main(string[] args)
+        {
+            Console.Clear();
+
+            var passedArgument = GetPassedArgument(args);
+
+
+
+            //DeployService();
+            //Finisher(1);
+
+
+        }
+
+        internal static string GetPassedArgument(string[] args) =>
+            args.Length == 0
+                ? Properties.Settings.Default.DefaultBranch
+                : Verification.PassedArguments.VerifyArguments(args[0])
+                    ? args[0]
+                    : "undefined";
+
+
+    }
+}
